851b77bbb39fdff06328a08336dc32729616e85f
AndryGP <andrea.damelio@gmail.com>
2015-06-23 11:49:44 +0200

    .

--- dataParser.js
+++ dataParser.js
@@ -3,22 +3,19 @@ var http = require('http'),
     url = require('url'),
     Converter=require("csvtojson").core.Converter;
 
-var teams = [];
 var salaries = [];
 var stats = [];
+var finalStats = [];
  
 var salaryFile="./salary.csv";
 var statsFile = "./team_season2000.csv";
-var teamsFile = "./teams.csv";
 
 var salaryFileStream = fs.createReadStream(salaryFile);
 var statsFileStream = fs.createReadStream(statsFile);
-var teamsFilStream = fs.createReadStream(teamsFile);
 
 //new converter instance 
 var salaryCsvConv = new Converter({constructResult:true, delimiter: ";"});
 var statsCsvConv = new Converter({constructResult:true});
-var teamsCsvConv = new Converter({constructResult:true});
 
 
 //end_parsed will be emitted once parsing finished 
@@ -29,36 +26,51 @@ salaryCsvConv.on("end_parsed",function(jsonObj){
 		salary.total = jsonObj[item].TOTAL;
 		salary.avg = jsonObj[item].AVG;
 		salary.year = jsonObj[item].YEAR;
+		salary.id = jsonObj[item].ID;
 		salaries.push(salary);
 
 	}
-	// console.log(salaries); 
-	parseTeams();
+	//console.log(salaries); 
+	parseStats();
 });
 
+function parseStats() {
 
-
-function parseTeams() {
-	teamsCsvConv.on("end_parsed", function(jsonObj) {
+	statsCsvConv.on("end_parsed", function(jsonObj){
 		for(item in jsonObj) {
-			if(jsonObj[item].leag === "N") {
-				var team = {};
-				team.name = jsonObj[item].team;
-				team.franchige = jsonObj[item].name;
-				console.log(team);
-				teams.push(team);
-			}
+			var stat = {};
+			stat.team_id = jsonObj[item].team;
+			stat.year = jsonObj[item].year;
+			stat.team_wins = jsonObj[item].won;
+			stat.team_loss = jsonObj[item].lost;
+			stat.ratio = Math.round((stat.team_wins/(stat.team_wins + stat.team_loss))*1000) / 1000;
+			stats.push(stat);
 		}
-		mergeTeams2Salaries();
+		// console.log(stats);
+		dataCrossing();
 	});
 }
 
 
-function mergeTeams2Salaries() {
-	for(var i = 0; i<)
-}
 
+function dataCrossing() {
+	for (var i = 0; i < salaries.length; i++) {
+		for(var j = 0; j < stats.length; j++) {
+			if(salaries[i].id === stats[j].team_id && salaries[i].year === stats[j].year) {
+				stats[j].salaryTot = salaries[i].total;
+				stats[j].salaryAvg = salaries[i].avg;
+			}
+		}
+	}
+	for(var j = 0; j < stats.length; j++) {
+		if(typeof stats[j].salaryTot !== 'undefined') {
+			finalStats.push(stats[j]);
+		}
+	}
+	console.log(finalStats);
+}
+		
 
 //read from file 
 salaryFileStream.pipe(salaryCsvConv);
-teamsFilStream.pipe(teamsCsvConv);
+statsFileStream.pipe(statsCsvConv);
